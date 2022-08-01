9d06c745da3d1a57f770047fd4a51e491c66d4a1
DESKTOP-77C0AMU\Admin <komal.dongare@ojassoft.in>
2022-08-01 17:21:36 +0530

    Added images and data

--- my-app-github-master/my-app-github-master/src/AppComponent/ImageComp.js
+++ my-app-github-master/my-app-github-master/src/AppComponent/ImageComp.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 function ImageComp() {
   return (
-    <div>
-         <img
-        src="https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy"
-        alt='Happy'
-      />
-    </div>   
+    <div>ImageComp</div>
   )
 }
 
--- my-app-github-master/my-app-github-master/src/AppComponent/UserDetailsComp.js
+++ my-app-github-master/my-app-github-master/src/AppComponent/UserDetailsComp.js
@@ -1,27 +1,65 @@
+import React ,{useState} from 'react'
+import axios from 'axios'
 
-const UserDetailsComp=()=>{
+function UserDetailsComp() {
+
+    const [user,setUser]= useState([])
+    const fetchUsers=()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((response)=>{
+           setUser(response.data)
+          // console.log(response);
+        })
+        }
     
-    let url="https://jsonplaceholder.typicode.com/users";
-    const FetchDataPromise=()=>{
-    fetch(url)
-    .then((r)=>{
-        return r.json();
-    })
-    .then((result)=>{
-        const newResult = result.filter(function(e){
-            return e.id<2;
-        });
-       console.log(newResult);
-     
-    });
-
-};
-FetchDataPromise();
-    return(
-<div>
-    <p>User Information</p>
-   
+  return (
+    <>
+    <div>
+    <button className='btn btn-primary' onClick={fetchUsers}>FetchUsers</button>
+    </div>
+   <div className='container'>
+    <div className='row'>
+       {
+        user.map((value)=>{
+            return(
+                /*<div className='col-4'>
+                <div className="card" style={{width: "18rem"}}>
+          <img src="https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy" className="card-img-top" alt="..."/>
+           <br/><div className="card-body">
+            <h1 className="card-title">{value.name}</h1>
+            <p className="card-text">Email : {value.email}</p>
+            <p className="card-text">Phone :{value.phone}</p>
+            <p className="card-text">Website : {value.website}</p>
+          </div>
+        </div>
+                </div>*/
+                <div className="card mb-3" style={{width: "18rem"}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy" className="card-img-top" className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+      <h1 className="card-title">{value.name}</h1>
+            <p className="card-text">Email : {value.email}</p>
+            <p className="card-text">Phone :{value.phone}</p>
+            <p className="card-text">Company : {value.company.name}</p>
+            <p className="card-text">Website : {value.website}</p>
+            <p className="card-text">Address : {value.address.street},{value.address.suite},{value.address.city},{value.address.zipcode}</p>
+           
+        
+      </div>
+    </div>
+  </div>
 </div>
-    );
-};
-export default UserDetailsComp;
+            )
+        })
+       }
+    </div>
+   </div>
+
+    </>
+  )
+  }
+
+export default UserDetailsComp
